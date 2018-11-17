c8c87084205a9c833655b43dfd1c6d925d796083
oooooba <fuumin.23@gmail.com>
2018-11-17 11:23:01 +0900

    feature: add feature to apply k-normalization for binary logic operations

--- src/MinCaml/KNormal.hs
+++ src/MinCaml/KNormal.hs
@@ -18,6 +18,14 @@ data T
         Id.T
   | Sub Id.T
         Id.T
+  | IfEq Id.T
+         Id.T
+         T
+         T
+  | IfLe Id.T
+         Id.T
+         T
+         T
   | Let (Id.T, Type.Type)
         T
         T
@@ -39,11 +47,36 @@ gBinOpHelper env e1 e2 c t = do
     p2 <- g env e2
     insertLet p2 $ \y -> return (c x y, t)
 
+gIfCmpHelper ::
+     Map.Map Id.T Type.Type
+  -> Syntax.T
+  -> Syntax.T
+  -> Syntax.T
+  -> Syntax.T
+  -> (Id.T -> Id.T -> T -> T -> T)
+  -> MinCaml (T, Type.Type)
+gIfCmpHelper env e1 e2 et ef c = do
+  p1 <- g env e1
+  insertLet p1 $ \v1 -> do
+    p2 <- g env e2
+    insertLet p2 $ \v2 -> do
+      (et', t3) <- g env et
+      (ef', _) <- g env ef
+      return (c v1 v2 et' ef', t3)
+
 g :: Map.Map Id.T Type.Type -> Syntax.T -> MinCaml (T, Type.Type)
-g _ Syntax.Unit          = return (Unit, Type.Unit)
-g _ (Syntax.Int i)       = return (Int i, Type.Int)
+g _ Syntax.Unit = return (Unit, Type.Unit)
+g _ (Syntax.Bool b) = return (Int $ fromEnum b, Type.Int)
+g _ (Syntax.Int i) = return (Int i, Type.Int)
+g env (Syntax.Not e) = g env $ Syntax.If e (Syntax.Bool False) (Syntax.Bool True)
 g env (Syntax.Add e1 e2) = gBinOpHelper env e1 e2 Add Type.Int
 g env (Syntax.Sub e1 e2) = gBinOpHelper env e1 e2 Sub Type.Int
+g env cmp@(Syntax.Eq _ _) = g env $ Syntax.If cmp (Syntax.Bool True) (Syntax.Bool False)
+g env cmp@(Syntax.Le _ _) = g env $ Syntax.If cmp (Syntax.Bool True) (Syntax.Bool False)
+g env (Syntax.If (Syntax.Not e1) e2 e3) = g env $ Syntax.If e1 e3 e2
+g env (Syntax.If (Syntax.Eq e1 e2) e3 e4) = gIfCmpHelper env e1 e2 e3 e4 IfEq
+g env (Syntax.If (Syntax.Le e1 e2) e3 e4) = gIfCmpHelper env e1 e2 e3 e4 IfLe
+g env (Syntax.If e1 e2 e3) = g env $ Syntax.If (Syntax.Eq e1 $ Syntax.Bool False) e3 e2
 
 f :: Syntax.T -> MinCaml T
 f e = fst <$> g Map.empty e
