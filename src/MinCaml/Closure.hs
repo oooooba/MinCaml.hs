1ebd4da1a445e6b50704b990e77d0cb8f17b5052
oooooba <fuumin.23@gmail.com>
2018-11-18 11:04:52 +0900

    feature: add feature to apply closure-conversion for binary operations

--- src/MinCaml/Closure.hs
+++ src/MinCaml/Closure.hs
@@ -1,5 +1,6 @@
 module MinCaml.Closure where
 
+import           Control.Monad              (liftM2)
 import           Control.Monad.State.Strict (State, get, modify, runState)
 import qualified Data.Map                   as Map
 import qualified Data.Set                   as Set
@@ -12,6 +13,21 @@ import qualified MinCaml.Type               as Type
 data T
   = Unit
   | Int Int
+  | Add Id.T
+        Id.T
+  | Sub Id.T
+        Id.T
+  | IfEq Id.T
+         Id.T
+         T
+         T
+  | IfLe Id.T
+         Id.T
+         T
+         T
+  | Let (Id.T, Type.Type)
+        T
+        T
   deriving (Show, Eq)
 
 data Fundef = Fundef
@@ -34,8 +50,13 @@ data ClosureStatus = ClosureStatus
 type MinCamlClosure a = State ClosureStatus a
 
 g :: Map.Map Id.T Type.Type -> Set.Set Id.T -> KNormal.T -> MinCamlClosure T
-g _ _ KNormal.Unit    = return Unit
+g _ _ KNormal.Unit = return Unit
 g _ _ (KNormal.Int i) = return $ Int i
+g _ _ (KNormal.Add x y) = return $ Add x y
+g _ _ (KNormal.Sub x y) = return $ Sub x y
+g env known (KNormal.IfEq x y e1 e2) = liftM2 (IfEq x y) (g env known e1) (g env known e2)
+g env known (KNormal.IfLe x y e1 e2) = liftM2 (IfLe x y) (g env known e1) (g env known e2)
+g env known (KNormal.Let (x, t) e1 e2) = liftM2 (Let (x, t)) (g env known e1) (g env known e2)
 
 f :: KNormal.T -> MinCaml Prog
 f e = do
--- test/MinCaml/ClosureSpec.hs
+++ test/MinCaml/ClosureSpec.hs
@@ -10,6 +10,7 @@ import           MinCaml.Global
 import qualified MinCaml.KNormal  as KNormal
 import qualified MinCaml.Lexer    as Lexer
 import qualified MinCaml.Parser   as Parser
+import qualified MinCaml.Type     as Type
 import qualified MinCaml.Typing   as Typing
 
 import           MinCaml.TestCase
@@ -29,3 +30,43 @@ spec =
     specHelper validCase2 $ Right $ Closure.Prog [] Closure.Unit
     specHelper validCase3 $ Right $ Closure.Prog [] (Closure.Int 42)
     specHelper validCase4 $ Right $ Closure.Prog [] (Closure.Int 42)
+    specHelper validCase5 $
+      Right $
+      Closure.Prog [] $
+      Closure.Let ("Ti0.0", Type.Int) (Closure.Int 1) $
+      Closure.Let ("Ti1.1", Type.Int) (Closure.Int 2) $ Closure.Add "Ti0" "Ti1"
+    specHelper validCase6 $
+      Right $
+      Closure.Prog [] $
+      Closure.Let ("Ti0.0", Type.Int) (Closure.Int 3) $
+      Closure.Let ("Ti1.1", Type.Int) (Closure.Int 4) $ Closure.Sub "Ti0" "Ti1"
+    specHelper validCase7 $
+      Right $
+      Closure.Prog [] $
+      Closure.Let ("Ti0.0", Type.Int) (Closure.Int 5) $
+      Closure.Let ("Ti1.1", Type.Int) (Closure.Int 6) $ Closure.IfEq "Ti0" "Ti1" (Closure.Int 1) (Closure.Int 0)
+    specHelper validCase8 $
+      Right $
+      Closure.Prog [] $
+      Closure.Let ("Ti0.0", Type.Int) (Closure.Int 7) $
+      Closure.Let ("Ti1.1", Type.Int) (Closure.Int 8) $ Closure.IfEq "Ti0" "Ti1" (Closure.Int 0) (Closure.Int 1)
+    specHelper validCase9 $
+      Right $
+      Closure.Prog [] $
+      Closure.Let ("Ti0.0", Type.Int) (Closure.Int 9) $
+      Closure.Let ("Ti1.1", Type.Int) (Closure.Int 10) $ Closure.IfLe "Ti0" "Ti1" (Closure.Int 1) (Closure.Int 0)
+    specHelper validCase10 $
+      Right $
+      Closure.Prog [] $
+      Closure.Let ("Ti0.0", Type.Int) (Closure.Int 12) $
+      Closure.Let ("Ti1.1", Type.Int) (Closure.Int 11) $ Closure.IfLe "Ti0" "Ti1" (Closure.Int 1) (Closure.Int 0)
+    specHelper validCase11 $
+      Right $
+      Closure.Prog [] $
+      Closure.Let ("Ti0.0", Type.Int) (Closure.Int 14) $
+      Closure.Let ("Ti1.1", Type.Int) (Closure.Int 13) $ Closure.IfLe "Ti0" "Ti1" (Closure.Int 0) (Closure.Int 1)
+    specHelper validCase12 $
+      Right $
+      Closure.Prog [] $
+      Closure.Let ("Ti0.0", Type.Int) (Closure.Int 15) $
+      Closure.Let ("Ti1.1", Type.Int) (Closure.Int 16) $ Closure.IfLe "Ti0" "Ti1" (Closure.Int 0) (Closure.Int 1)
