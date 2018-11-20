e45b13a6d032c6e2d99e50f781655d87b0edc3e9
oooooba <fuumin.23@gmail.com>
2018-11-20 21:55:24 +0900

    feature: add helper functions to apply immediate optimization

--- src/MinCaml/Asm.hs
+++ src/MinCaml/Asm.hs
@@ -1,5 +1,7 @@
 module MinCaml.Asm where
 
+import qualified Data.Set     as Set
+
 import qualified MinCaml.Id   as Id
 import qualified MinCaml.Type as Type
 
@@ -45,6 +47,30 @@ data Prog =
        [Fundef]
        T
   deriving (Show, Eq)
+
+removeAndUniq :: Ord a => Set.Set a -> [a] -> [a]
+removeAndUniq _ [] = []
+removeAndUniq xs (x:ys)
+  | x `Set.member` xs = removeAndUniq xs ys
+removeAndUniq xs (x:ys) = removeAndUniq (Set.insert x xs) ys
+
+fvIdOrImm :: IdOrImm -> [Id.T]
+fvIdOrImm (V x) = [x]
+fvIdOrImm _     = []
+
+fvExp :: Exp -> [Id.T]
+fvExp (Set _) = []
+fvExp (Add x y') = x : fvIdOrImm y'
+fvExp (Sub x y') = x : fvIdOrImm y'
+fvExp (IfEq x y' e1 e2) = x : fvIdOrImm y' ++ removeAndUniq Set.empty (fvHelper e1 ++ fvHelper e2)
+fvExp (IfLe x y' e1 e2) = x : fvIdOrImm y' ++ removeAndUniq Set.empty (fvHelper e1 ++ fvHelper e2)
+
+fvHelper :: T -> [Id.T]
+fvHelper (Ans exp) = fvExp exp
+fvHelper (Let (x, _) exp e) = fvExp exp ++ removeAndUniq (Set.singleton x) (fvHelper e)
+
+fv :: T -> [Id.T]
+fv e = removeAndUniq Set.empty $ fvHelper e
 
 concat :: T -> (Id.T, Type.Type) -> T -> T
 concat (Ans exp) xt e        = Let xt exp e
