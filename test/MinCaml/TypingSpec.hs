92b7aba1a068bb3166c9bae0562add30ee06f6be
oooooba <fuumin.23@gmail.com>
2018-11-25 10:00:52 +0900

    feature: add test to type for boolean literal and if expression

--- test/MinCaml/TypingSpec.hs
+++ test/MinCaml/TypingSpec.hs
@@ -73,3 +73,5 @@ spec = do
     specHelper validCase13 $ Right (Syntax.Let ("x_", Type.Int) (Syntax.Int 42) (Syntax.Var "x_"), Type.Int)
     specHelper validCase14 $
       Right (Syntax.Eq (Syntax.Neg $ Syntax.Int 1) (Syntax.Sub (Syntax.Neg $ Syntax.Int 2) (Syntax.Int 3)), Type.Bool)
+    specHelper validCase15 $
+      Right (Syntax.If (Syntax.Bool True) (Syntax.Bool False) (Syntax.Not $ Syntax.Bool True), Type.Bool)
