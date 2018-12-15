2d740c02a8a418a1ae211f1bf3533e10b272047e
oooooba <fuumin.23@gmail.com>
2018-12-15 17:04:41 +0900

    refactoring: modify to use GlobalStatus in register allocation

--- src/MinCaml/RegAlloc.hs
+++ src/MinCaml/RegAlloc.hs
@@ -2,14 +2,17 @@ module MinCaml.RegAlloc
   ( f
   ) where
 
-import           Control.Exception (assert)
-import           Data.Either       (fromRight)
-import qualified Data.Map          as Map
+import           Control.Exception          (assert)
+import           Control.Monad.Except       (ExceptT, runExceptT, throwError)
+import           Control.Monad.Identity     (Identity, runIdentity)
+import           Control.Monad.State.Strict (StateT, get, put, runStateT)
+import           Data.Either                (fromRight)
+import qualified Data.Map                   as Map
 
-import qualified MinCaml.Asm       as Asm
+import qualified MinCaml.Asm                as Asm
 import           MinCaml.Global
-import qualified MinCaml.Id        as Id
-import qualified MinCaml.Type      as Type
+import qualified MinCaml.Id                 as Id
+import qualified MinCaml.Type               as Type
 
 data Exc
   = NotFound
@@ -17,7 +20,7 @@ data Exc
           Type.Type
   deriving (Show, Eq)
 
-type MinCamlRegAlloc a = Either Exc a
+type MinCamlRegAlloc a = StateT GlobalStatus (ExceptT Exc Identity) a
 
 type RegEnv = Map.Map Id.T Id.T
 
@@ -60,13 +63,14 @@ find x _ regenv
   | Asm.isReg x = return x
 find x _ regenv
   | Map.member x regenv = return $ regenv Map.! x
-find x t _ = Left $ NoReg x t
+find x t _ = throwError $ NoReg x t
 
 gAuxAndRestore :: (Id.T, Type.Type) -> Asm.T -> RegEnv -> Asm.Exp -> MinCamlRegAlloc (Asm.T, RegEnv)
-gAuxAndRestore dest cont regenv exp =
-  case gAux dest cont regenv exp of
+gAuxAndRestore dest cont regenv exp = do
+  globalStatus <- get
+  case run (gAux dest cont regenv exp) globalStatus of
     Left (NoReg x t) -> g dest cont regenv $ Asm.Let (x, t) (Asm.Restore x) $ Asm.Ans exp
-    result@(Right _) -> result
+    Right (result, globalStatus') -> put globalStatus' >> return result
 
 gAux :: (Id.T, Type.Type) -> Asm.T -> RegEnv -> Asm.Exp -> MinCamlRegAlloc (Asm.T, RegEnv)
 gAux dest cont regenv exp@Asm.Nop     = return (Asm.Ans exp, regenv)
@@ -76,15 +80,24 @@ g :: (Id.T, Type.Type) -> Asm.T -> RegEnv -> Asm.T -> MinCamlRegAlloc (Asm.T, Re
 g dest cont regenv (Asm.Ans exp) = gAuxAndRestore dest cont regenv exp
 g dest cont regenv (Asm.Let xt@(x, t) exp e) = undefined
 
--- ToDo: modify to use Global.genVar to make fresh variable
-neverUsedIdentifier :: String
-neverUsedIdentifier = "'never used identifier'"
-
 h :: Asm.Fundef -> MinCamlRegAlloc Asm.Fundef
 h = undefined
 
+run :: MinCamlRegAlloc a -> GlobalStatus -> Either Exc (a, GlobalStatus)
+run e s = runIdentity (runExceptT $ runStateT e s)
+
 f :: Asm.Prog -> MinCaml Asm.Prog
 f (Asm.Prog fdata fundefs e) = do
-  let fundefs' = fromRight undefined $ mapM h fundefs
-  let (e', _) = fromRight undefined $ g (neverUsedIdentifier ++ show 0, Type.Unit) (Asm.Ans Asm.Nop) Map.empty e
-  Prelude.return $ Asm.Prog fdata fundefs' e'
+  tmp <- genVar Type.Unit
+  globalStatus <- get
+  let (fundefs', globalStatus') =
+        foldl
+          (\(fundefs', gs) fundef ->
+             let (fundef', gs') = fromRight undefined (run (h fundef) gs)
+             in (fundef' : fundefs', gs'))
+          ([], globalStatus)
+          fundefs
+  let ((e', _), globalStatus'') =
+        fromRight undefined $ flip run globalStatus' $ g (tmp, Type.Unit) (Asm.Ans Asm.Nop) Map.empty e
+  put globalStatus''
+  Prelude.return $ Asm.Prog fdata (reverse fundefs') e'
