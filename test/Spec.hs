90d16a7d5343fd0b255770aec9ffa5b957285aca
oooooba <fuumin.23@gmail.com>
2019-01-10 16:59:50 +0900

    refactoring: modify Spec.hs to call spec functions explicitly

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,1 +1,25 @@
-{-# OPTIONS_GHC -F -pgmF hspec-discover #-}
+import           Test.Hspec
+
+import qualified MinCaml.AlphaSpec    as AlphaSpec
+import qualified MinCaml.ClosureSpec  as ClosureSpec
+import qualified MinCaml.EmitSpec     as EmitSpec
+import qualified MinCaml.KNormalSpec  as KNormalSpec
+import qualified MinCaml.LexerSpec    as LexerSpec
+import qualified MinCaml.ParserSpec   as ParserSpec
+import qualified MinCaml.RegAllocSpec as RegAllocSpec
+import qualified MinCaml.SimmSpec     as SimmSpec
+import qualified MinCaml.TypingSpec   as TypingSpec
+import qualified MinCaml.VirtualSpec  as VirtualSpec
+
+main :: IO ()
+main = do
+  hspec LexerSpec.spec
+  hspec ParserSpec.spec
+  hspec TypingSpec.spec
+  hspec KNormalSpec.spec
+  hspec AlphaSpec.spec
+  hspec ClosureSpec.spec
+  hspec VirtualSpec.spec
+  hspec SimmSpec.spec
+  hspec RegAllocSpec.spec
+  hspec EmitSpec.spec
